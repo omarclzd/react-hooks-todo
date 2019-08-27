8f3753b5d386f006807f72ee760659220458f95a
omar <omasrt@gmail.com>
2019-08-26 21:06:58 -0500

    sets Form component

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ addTodo }) => {
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setTodo("");
+    console.log(todo);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todo}
+          onChange={e => setTodo(e.currentTarget.value)}
+        />
+        <button>Sub</button>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
